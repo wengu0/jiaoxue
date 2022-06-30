bf475c7757c99a3587e585f4373c41bfe52e5ee1
wengu01 <2283758898@qq.com>
2022-06-30 13:12:15 +0800

    新增的精确验证

--- classes/classes_add_handler.php
+++ classes/classes_add_handler.php
@@ -17,28 +17,30 @@
 		 
 		 else{
 			//检查数据库是否有班级
-		 $check_sql= "select * from classes where cls_name='$cls_name'";
-		 $check=mysqli_query($conn,$check_sql);
-		 if($check->num_rows>0){
-			echo "<script>alert('已有班级，请重新输入');window.location.href='classes_add.html';</script>"; 
-		 }
-		 else{
-			$sql="INSERT INTO `classes`(`cls_id`, `cls_name`, `cls_count`, `enrollment_year`, `specialty_name`) ";
-			$sql.=" VALUES (null,'".$cls_name."','".$cls_count."','".$enrollment_year."','".$specialty_name."')";		
-		   
-			$result=mysqli_query($conn,$sql);				 
-   
-			//php中，非0值，默认为true
-			if($result>0)  
-			{		 	
-			   echo "<script>alert('保存成功');window.location.href='classes_info.html';</script>"; 
+			$check_sql= "select * from classes where cls_name='".$cls_name."'";
+			$check_sql.="and enrollment_year='".$enrollment_year."'";
+			$check_sql.="and specialty_name='".$specialty_name."'";
+			$check=mysqli_query($conn,$check_sql);
+			if($check->num_rows>0){
+			   echo "<script>alert('已有班级，请重新输入');window.location.href='classes_info.html';</script>"; 
+		   }
+			else{
+				$sql="INSERT INTO `classes`(`cls_id`, `cls_name`, `cls_count`, `enrollment_year`, `specialty_name`) ";
+				$sql.=" VALUES (null,'".$cls_name."','".$cls_count."','".$enrollment_year."','".$specialty_name."')";		
+			
+				$result=mysqli_query($conn,$sql);				 
+	
+				//php中，非0值，默认为true
+				if($result>0)  
+				{		 	
+				echo "<script>alert('保存成功');window.location.href='classes_info.html';</script>"; 
+				}
+				else
+				{
+				echo "<script>alert('保存失败');window.location.href='classes_add.html';</script>";  
+				} 
 			}
-			else
-			{
-			   echo "<script>alert('保存失败');window.location.href='classes_add.html';</script>";  
-			} 
-		}
-		
+			
 	} 
 }
     mysqli_close($conn);
--- course/course_add_handler.php
+++ course/course_add_handler.php
@@ -15,26 +15,28 @@
 		 }
 		 
 		 else{
-			$check_sql= "select * from course where course_name='$course_name'";
+			$check_sql= "select * from course where course_name='$course_name' ";
+			$check_sql.="and course_period='".$course_period."' ";
+			$check_sql.="and course_credit='".$course_credit."' ";
 			$check=mysqli_query($conn,$check_sql);
-		 if($check->num_rows>0){
-			echo "<script>alert('已有课程，请重新输入');window.location.href='course_info.php';</script>"; 
-		 }
-		 else{
-		 $sql="INSERT INTO `course`(`course_id`, `course_name`, `course_period`, `course_credit`) ";
-         $sql.=" VALUES (null,'".$course_name."','".$course_period."','".$course_credit."')";		
-        
-		 $result=mysqli_query($conn,$sql);				 
-         //php中，非0值，默认为true
-		 if($result>0)  
-		 {		 	
-			echo "<script>alert('保存成功');window.location.href='course_info.php';</script>"; 
-		 }
-		 else
-		 {
-			echo "<script>alert('保存失败');window.location.href='course_add.html';</script>";  
-		 } 
-	} 
+			if($check->num_rows>0){
+				echo "<script>alert('已有课程，请重新输入');window.location.href='course_info.html';</script>"; 
+			}
+			else{
+			$sql="INSERT INTO `course`(`course_id`, `course_name`, `course_period`, `course_credit`) ";
+			$sql.=" VALUES (null,'".$course_name."','".$course_period."','".$course_credit."')";		
+			
+			$result=mysqli_query($conn,$sql);				 
+			//php中，非0值，默认为true
+			if($result>0)  
+			{		 	
+				echo "<script>alert('保存成功');window.location.href='course_info.html';</script>"; 
+			}
+			else
+			{
+				echo "<script>alert('保存失败');window.location.href='course_add.html';</script>";  
+			} 
+		} 
     
 }
 	}mysqli_close($conn);
